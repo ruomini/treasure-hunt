ad2d8c6dbff43517729e19ca5c371444644303af
Rachel <ruomini@ma.org>
2016-06-15 19:27:05 +0000

    Better game logic and UI

--- client/js/base.js
+++ client/js/base.js
@@ -1,17 +1,19 @@
 function MyController($scope, $http) {
-    
+
     $scope.grid = [];
     $scope.color = '';
     $scope.message = '';
     $scope.done = false;
-    
+
     $scope.postData = function() {
-        $http.post('/api/v1/postData', {color: $scope.color}).then(function(response) {
+        $http.post('/api/v1/postData', {
+            color: $scope.color
+        }).then(function(response) {
             $scope.message = response.data.message;
         });
         $scope.color = '';
     };
-    
+
     $scope.getData = function() {
         $http.get('/api/v1/getData').then(function(response) {
             $scope.grid = response.data;
@@ -19,44 +21,55 @@ function MyController($scope, $http) {
             $scope.done = false;
         })
     };
-    
+
     var treasures = 0;
     var orangutans = 0;
+    var highScore = 100;
 
     $scope.clickTree = function(cell) {
-        if($scope.done == false){
-            $scope.grid[cell.row][cell.col].clicked=true;
-        if(cell.hasTreasure){
-            treasures++;
-        }
-        if(cell.hasOrangutan){
-            orangutans++;
-        }
-        console.log("Treasures found: " + treasures + "/10");
-            if(treasures == 10){
+        if ($scope.done == false && !$scope.grid[cell.row][cell.col].clicked) {
+            $scope.grid[cell.row][cell.col].clicked = true;
+            if (cell.hasTreasure) {
+                treasures++;
+                console.log("Treasures found: " + treasures + "/10");
+            }
+            if (cell.hasOrangutan) {
+                orangutans++;
+            }
+
+            if (treasures == 10) {
                 $scope.done = true;
                 alert("game over");
                 treasures = 0;
                 alert("orangutans destroyed: " + orangutans);
+                console.log("orangutans destroyed: " + orangutans);
+                var newScore = orangutans;
+                if(newScore < highScore){
+                    highScore = newScore;
+                }
+                alert("high score: " + highScore);
+                console.log("high score: " + highScore);
                 orangutans = 0;
             }
         }
-        
+
     };
-    
+
     $scope.imageDisplay = function(cell) {
         var className = 'tree';
         if ($scope.grid[cell.row][cell.col].clicked) {
             if (cell.hasOrangutan) {
                 className = 'orangutan';
-            } else if (cell.hasTreasure) {
+            }
+            else if (cell.hasTreasure) {
                 className = 'treasure';
-            } else {
+            }
+            else {
                 className = '';
             }
-            
+
         }
-                
+
         return className;
     }
 }
